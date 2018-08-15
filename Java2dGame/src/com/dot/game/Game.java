13b8f41222b47fc68c2781892c88e90495dcaeb8
Metro <Metro@DESKTOP-1JEI530>
2018-08-15 17:02:27 -0600

    Side scrolling sprites

--- Java2dGame/src/com/dot/game/Game.java
+++ Java2dGame/src/com/dot/game/Game.java
@@ -10,122 +10,130 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.dot.game.gfx.Screen;
 import com.dot.game.gfx.SpriteSheet;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
 	public static final int WIDTH = 160;
-	public static final int HEIGHT = WIDTH / 12*9;
+	public static final int HEIGHT = WIDTH / 12 * 9;
 	public static final int SCALE = 3;
 	public static final String NAME = "Game";
-	
+
 	private JFrame frame;
 	public boolean running = false;
 	public int tickCount = 0;
-	
+
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
-	private SpriteSheet spriteSheet = new SpriteSheet("/sprite_sheet.png");
-	
-	
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+	private Screen screen;
+
 	public Game() {
-		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 
 		frame = new JFrame(NAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
-		
-		//adding the canvas to the frame
+
+		// adding the canvas to the frame
 		frame.add(this, BorderLayout.CENTER);
 		frame.pack();
-		
+
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);		
+		frame.setVisible(true);
 	}
-	
+
+	public void init() {
+		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+	}
+
 	public synchronized void start() {
 		running = true;
 		new Thread(this).start();
-		
+
 	}
-	
+
 	public synchronized void stop() {
-			running = false;
-		}
-	
+		running = false;
+	}
+
 	public void run() {
 		long lastTime = System.nanoTime();
-		double nsPerTick = 1000000000D/60D;
-		
+		double nsPerTick = 1000000000D / 60D;
+
 		int ticks = 0;
 		int frames = 0;
-		
+
 		long lastTimer = System.currentTimeMillis();
 		double delta = 0;
-		
-		while(running) {
+
+		init();
+
+		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / nsPerTick;
 			lastTime = now;
 			boolean shouldRender = true;
-			
-			while(delta >= 1) {
+
+			while (delta >= 1) {
 				ticks++;
 				tick();
 				delta -= 1;
 				shouldRender = true;
 			}
-			
+
 			try {
 				Thread.sleep(2);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if(shouldRender) {
+			if (shouldRender) {
 				frames++;
 				render();
 			}
-			
-			
-			if(System.currentTimeMillis() - lastTimer >= 1000) {
+
+			if (System.currentTimeMillis() - lastTimer >= 1000) {
 				lastTimer += 1000;
-				System.out.println(frames+" Frames, " + ticks+" Ticks!");
+				System.out.println(frames + " Frames, " + ticks + " Ticks!");
 				frames = 0;
 				ticks = 0;
 			}
 		}
 	}
-	
+
 	public void tick() {
 		tickCount++;
+		screen.xOffset++;
 		
-		for(int i = 0; i < pixels.length; i++) {
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = i + tickCount;
 		}
+		
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
-			createBufferStrategy(3);//Triple Buffering
+		if (bs == null) {
+			createBufferStrategy(3);// Triple Buffering
 			return;
 		}
-		
+
+		screen.render(pixels, 0, WIDTH);
+
 		Graphics g = bs.getDrawGraphics();
-		
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
-	
+
 	public static void main(String[] args) {
 		new Game().start();
 	}
-	
+
 }
